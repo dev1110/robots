00be40a64aecf3d9cb20690ff00c987e8f54444c
lata-clabs <lata@cyberlabsindia.com>
2021-07-01 14:55:50 +0530

    minor update in card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = (props)=>{
     const {id, name, email} = props;
     return(
         <div className="card dib br3 tc ma2 pa2 grow bw2 shadow-5">
-            <img src={`https://robohash.org/${id}/?set=set4&size=220x220`} alt="robo"/>
+            <img src={`https://robohash.org/${id}/?set=set1&size=220x220`} alt="robo"/>
             <div>
                 <h2>{name}</h2>
                 <p> {email} </p>
